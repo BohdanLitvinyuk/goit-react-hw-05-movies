67b437a957d028106efa80ea8b135492675ad9a4
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-08-24 22:37:03 +0200

    styled

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,41 @@
+import "./MovieCard.css"
+
+const MovieCard = ({ movie }) => {
+  const {
+    original_title,
+    overview,
+    vote_average,
+    genres,
+    poster_path,
+    tagline,
+  } = movie;
+
+  return (
+    <div className="movieCard">
+      <img
+        src={`${
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+            : movie.original_title
+        }`}
+        alt={tagline} height="320"
+      />
+      <div>
+        <h1>{original_title}</h1>
+      <p> User Score: {Math.round(vote_average)}%</p>
+      <h2> Overview</h2>
+      <p>{overview}</p>
+
+      <ul>
+        <h3>Genres:</h3>
+        {genres?.map(genre => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+      <p>Additional Information</p>
+      </div>
+      
+    </div>
+  );
+};
+export default MovieCard;
--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,39 +2,36 @@ import { useRef, useState, useEffect } from 'react';
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import { getMovieById } from 'getFetch';
 // import { getMovieById } from 'getFetch';
+import MovieCard from './MovieCard/MovieCard';
 
 const MovieDetails = () => {
-const { movieId } = useParams();
-const [movie, setMovie] = useState({});
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState({});
   const location = useLocation();
 
   const backLinkLocationRef = useRef(location.state?.from ?? '/movies');
 
-  
   useEffect(() => {
     const fetchMovieById = async () => {
-       
       try {
         const movieById = await getMovieById(movieId);
         // console.log(movieById);
         setMovie(movieById);
-       
-
       } catch (error) {
         console.log(error);
       }
     };
     fetchMovieById();
-    
   }, [movieId]);
-  const BASE_POSTER_URL = 'https://image.tmdb.org/t/p/w500/';
+
   return (
     <>
-      MovieDetails : {movieId}
-      <Link to={backLinkLocationRef.current}> Back to movie</Link>
-      <img src={`${ movie.poster_path
-              ? BASE_POSTER_URL + movie.poster_path :  movie.original_title}`} alt={`${movie.title}`}/>
-      
+      <Link to={backLinkLocationRef.current}>
+        <button>Go Back </button>
+      </Link>
+
+      <MovieCard movie={movie} />
+
       <ul>
         <li>
           <Link to="Cast"> Cast</Link>
